48c22ff7a2f549222f17142eefdf78b0f8e05561
jpranays <pranay1315@gmail.com>
2024-09-14 20:02:02 +0530

    feat: Add terminal simulation component and styles

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -7,7 +7,9 @@ import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
 import { useCanvasAnimation } from "../hooks/useCanvasAnimation";
-
+import Termynal from "./terminal";
+import { useEffect } from "react";
+import "./terminal.css";
 function HomepageHeader() {
 	const { siteConfig } = useDocusaurusContext();
 	return (
@@ -17,13 +19,44 @@ function HomepageHeader() {
 					{siteConfig.title}
 				</Heading>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<GHCli />
 				<div className={styles.buttons}>
 					<Link className="button button--primary button--lg" to="/docs/intro">
-						Get started
+						See all commands
 					</Link>
 				</div>
 			</div>
 		</header>
+	);
+}
+
+function GHCli() {
+	useEffect(() => {
+		new Termynal("#termynal");
+	}, []);
+	return (
+		<div
+			id="termynal"
+			data-termynal
+			data-ty-typeDelay="40"
+			data-ty-lineDelay="700"
+		>
+			<span data-ty="input" data-color="green">
+				ghc repo-create
+			</span>
+			<span data-ty="input" data-ty-prompt="?" data-color="white">
+				Enter the repository name: Test-Repo
+			</span>
+			<span data-ty="input" data-ty-prompt="?" data-color="white">
+				Enter the repository description: This is a test repository
+			</span>
+			<span data-ty="input" data-ty-prompt="?" data-color="white">
+				Should the repository be private? (y/n): n
+			</span>
+			<span data-ty data-color="green">
+				Repository 'Test-Repo' created successfully!
+			</span>
+		</div>
 	);
 }
 
