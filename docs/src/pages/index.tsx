8837046c3d1c7e0157e7fd1319dac9b0b44e2e10
jpranays <pranay1315@gmail.com>
2025-04-20 21:34:23 +0530

    Hero-title css changes

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -104,7 +104,7 @@ function HomepageHeader() {
 	return (
 		<header className={clsx("hero hero--primary", styles.heroBanner)}>
 			<div className="container">
-				<Heading as="h1" className="hero__title">
+				<Heading as="h1" className="hero__title custom-hero-title">
 					{siteConfig.title}
 				</Heading>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
